ea57017fa4edbeed4f27f51803c217d4beb05bc1
ToneUs <tone.Us0w0@gmail.com>
2023-04-10 14:12:11 +0900

    013

---
+++ templete/Dijkstra.py
@@ -0,0 +1,29 @@
+import heapq
+INF = 10 ** 9
+# ----------------------------------------------------------------
+# Input
+#   1. タプル(重み, 行先)の二次元配列(隣接リスト)
+#   2. 探索開始ノード(番号)
+# Output
+#   スタートから各ノードへの最小コスト
+# ----------------------------------------------------------------
+def dijkstra(edges: "List[List[(cost, to)]]", start_node: int) -> list:
+    hq = []
+    heapq.heapify(hq)
+    # Set start info
+    dist = [INF] * len(edges)
+    heapq.heappush(hq, (0, start_node))
+    dist[start_node] = 0
+    # dijkstra
+    while hq:
+        # ------------------------------------------- タイミング1
+        min_cost, now = heapq.heappop(hq)
+        # ------------------------------------------- タイミング2
+        if min_cost > dist[now]:
+            continue
+        for cost, next in edges[now]:
+            if dist[next] > dist[now] + cost:
+                dist[next] = dist[now] + cost
+                heapq.heappush(hq, (dist[next], next))
+            # --------------------------------------- タイミング3
+    return dist
